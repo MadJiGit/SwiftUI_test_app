5cfa6224e1b640f12bb0c88b43ec6fb34cbbb6ce
MadJiGit <raykovmladen@gmail.com>
2020-07-22 14:38:34 +0300

    first stage of app

---
+++ SwiftUI_test_app/Circle.swift
@@ -0,0 +1,21 @@
+//
+//  Circle.swift
+//  SwiftUI_test_app
+//
+//  Created by Madji on 22.07.20.
+//  Copyright © 2020 Madji. All rights reserved.
+//
+
+import SwiftUI
+
+struct Circle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Circle_Previews: PreviewProvider {
+    static var previews: some View {
+        Circle()
+    }
+}
