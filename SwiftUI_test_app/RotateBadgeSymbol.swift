346a42b8dbb98fc1477209762344319c95a1d9a6
MadJiGit <raykovmladen@gmail.com>
2020-07-23 16:43:53 +0300

    add badge

---
+++ SwiftUI_test_app/RotateBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotateBadgeSymbol.swift
+//  SwiftUI_test_app
+//
+//  Created by Madji on 23.07.20.
+//  Copyright © 2020 Madji. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+        .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
